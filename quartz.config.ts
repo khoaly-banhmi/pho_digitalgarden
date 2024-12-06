570a2ff9b8ca560c7bafd667dd7ea0345c14731b
khoaly-banhmi <lynhatkhoa17@gmail.com>
2024-12-05 21:59:09 -0600

    Quartz sync: Dec 5, 2024, 9:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🍜 Pho Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "DM Sans",
+        body: "DM Sans",
         code: "IBM Plex Mono",
       },
       colors: {
