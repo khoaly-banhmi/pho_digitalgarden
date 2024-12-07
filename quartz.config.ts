232a30372200c28bd0435523e41d5b37ff90f523
khoaly-banhmi <lynhatkhoa17@gmail.com>
2024-12-07 11:28:31 -0600

    Quartz sync: Dec 7, 2024, 11:28 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "vi-VN",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
